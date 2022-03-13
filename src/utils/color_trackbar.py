f5342ab2ea8a62f8bbf239a201adb39f0368b9be
Chien Yü <Conjure.li@gmail.com>
2022-03-13 10:10:12 +0800

    change the way to store json hsv values
    Signed-off-by: Chien Yü <Conjure.li@gmail.com>

--- src/utils/color_trackbar.py
+++ src/utils/color_trackbar.py
@@ -50,9 +50,25 @@ class Trackbar:
         self.V_h = val
         self.in_range()
 
-def save_values(color_low,color_high):
+def save_values(color_low,color_high,img_path):
     with open("config.json",'r') as f:
         config = json.load(f)
+        img_name = os.path.split(img_path)[-1]
+        config[img_name] = {
+            'color':
+                {
+                    'low':
+                        {
+                            "H":color_low[0], 'S':color_low[1],'V':color_low[2]
+                        },
+                    'high':
+                        {
+                            "H": color_high[0], 'S': color_high[1], 'V': color_high[2]
+                        }
+                }
+        }
+        with open("config.json",'w') as f:
+            json.dump(config,f,indent=4)
         config["color"]["low"] = {"H": color_low[0], "S": color_low[1], "V": color_low[2]}
         config["color"]["high"] = {"H": color_high[0], "S": color_high[1], "V": color_high[2]}
         with open("config.json", "w") as f:
@@ -76,7 +92,7 @@ def color_trackbar(img_path):
     while True:
         k = cv.waitKey(0)
         if k == ord('s'):
-            save_values(t.color_low,t.color_high)
+            save_values(t.color_low,t.color_high,img_path)
         elif k == 27:
             cv.destroyAllWindows()
             break
